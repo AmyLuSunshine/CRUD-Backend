8250ce197bcb52aa5e822f922e30545e7be2576f
Yangmei (Amy) Lu <109566558+AmyLuSunshine@users.noreply.github.com>
2025-07-02 23:07:40 -0400

    first commit

--- api/index.js
+++ api/index.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const ducksRouter = require("./ducks");
+//const ducksRouter = require("./ducks");
+const campusRouter = require("./campus");
+const studentRouter = require("./student");
 
-router.use("/ducks", ducksRouter);
+//router.use("/ducks", ducksRouter);
+router.use("/students", studentRouter);
+router.use("/campuses", campusRouter);
 
 module.exports = router;
